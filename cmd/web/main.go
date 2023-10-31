b8fb727bd56626704eb900077b94b27cad4f5429
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-10-31 13:53:42 +0400

    add signin & signup routes with templates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -25,11 +25,21 @@ func aboutDir(w http.ResponseWriter, r *http.Request) {
 	rnd.HTML(w, http.StatusOK, "about", nil)
 }
 
+func signInDir(w http.ResponseWriter, r *http.Request) {
+	rnd.HTML(w, http.StatusOK, "signin", nil)
+}
+
+func signUpDir(w http.ResponseWriter, r *http.Request) {
+	rnd.HTML(w, http.StatusOK, "signup", nil)
+}
+
 func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", homeDir)
 	mux.HandleFunc("/about", aboutDir)
+	mux.HandleFunc("/signin", signInDir)
+	mux.HandleFunc("/signup", signUpDir)
 
 	log.Println("Server starting on: 8080")
 
