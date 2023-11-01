ef21addda2bcf5e2745376a1a1f4131b10394296
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-11-01 18:17:34 +0400

    create helper function to improve app development

--- cmd/web/utils.go
+++ cmd/web/utils.go
@@ -9,20 +9,25 @@ import (
 	"github.com/go-playground/form/v4"
 )
 
-func (app *application) serverError(res http.ResponseWriter, err error) {
+func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
-	app.errorLog.Print(trace)
+	app.errorLog.Output(2, trace)
 
-	http.Error(res, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	if app.debug {
+		http.Error(w, trace, http.StatusInternalServerError)
+		return
+	}
+
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
 
 func (app *application) clientError(res http.ResponseWriter, status int) {
 	http.Error(res, http.StatusText(status), status)
 }
 
-func (app *application) notFound(res http.ResponseWriter) {
-	app.clientError(res, http.StatusNotFound)
-}
+// func (app *application) notFound(res http.ResponseWriter) {
+// 	app.clientError(res, http.StatusNotFound)
+// }
 
 func (app *application) decodePostForm(res *http.Request, decode any) error {
 	err := res.ParseForm()
@@ -42,4 +47,17 @@ func (app *application) decodePostForm(res *http.Request, decode any) error {
 	}
 
 	return nil
+}
+
+func (app *application) newTemplateData(req *http.Request) *templateData {
+	return &templateData{}
+}
+
+func (app *application) isAuthenticated(r *http.Request) bool {
+	isAuthenticated, ok := r.Context().Value(isAuthenticatedContextKey).(bool)
+	if !ok {
+		return false
+	}
+
+	return isAuthenticated
 }
